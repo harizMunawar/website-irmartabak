bfffcd7a51d1f48a980c5a556182d580b427803b
harizMunawar <munawarhariz@gmail.com>
2020-08-04 14:19:59 +0700

    Fixing broken image link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/img/logo.png">
     <HelloWorld msg="This is a vue app"/>
   </div>
 </template>
